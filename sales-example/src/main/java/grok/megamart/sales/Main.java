01f4b2375b4b2d1e118e0b5fdc483d5626b202b0
Евгений Терехов <nergal@psu.ru>
2021-08-08 21:00:56 +0500

    Calculating free shipping

--- sales-example/src/main/java/grok/megamart/sales/Main.java
+++ sales-example/src/main/java/grok/megamart/sales/Main.java
@@ -3,14 +3,24 @@ package grok.megamart.sales;
 import grok.megamart.domain.Item;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Main {
     private static List<Item> shoppingCart = new ArrayList<>();
     private static double shoppingCartTotal;
+    private static Map<Item, Boolean> shippingInfo = new HashMap<>();
+
+    static {
+        shippingInfo.put(new Item("Футболка", 5.00), false);
+        shippingInfo.put(new Item("Кроссовки", 12.00), false);
+        shippingInfo.put(new Item("Галстук", 8.00), false);
+    }
 
     public static void main(String[] args) {
-        addItemToCart("Футболка", 5.00);
+        addItemToCart("Джинсы", 10.00);
     }
 
     private static void addItemToCart(String name, double price) {
@@ -24,18 +34,31 @@ public class Main {
             Item item = shoppingCart.get(i);
             shoppingCartTotal += item.getPrice();
         }
-        logAvailableItems();
+        updateShippingInfo();
         logCartTotal();
+        logAvailableItems();
+    }
+
+    private static void updateShippingInfo() {
+        for (Item item : shippingInfo.keySet()) {
+            if (item.getPrice() + shoppingCartTotal >= 20) {
+                shippingInfo.put(item, true);
+            } else {
+                shippingInfo.put(item, false);
+            }
+        }
     }
 
     private static void logCartTotal() {
+        System.out.printf("Общая стоимость товаров в корзине: %3.2f%n", shoppingCartTotal);
         System.out.println("-----");
-        System.out.printf("Общая стоимость товаров в корзине: %3.2f", shoppingCartTotal);
     }
 
     private static void logAvailableItems() {
-        System.out.printf("%-10s:%7.2f%n", "Футболка", 5.0);
-        System.out.printf("%-10s:%7.2f%n", "Кроссовки", 12.0);
-        System.out.printf("%-10s:%7.2f%n", "Галстук", 8.0);
+        for (Item item : shippingInfo.keySet()) {
+            System.out.printf("%-10s:%7.2f\t%s%n",
+                    item.getName(), item.getPrice(),
+                    shippingInfo.get(item) ? "Бесплатная доставка!!!" : "");
+        }
     }
 }
